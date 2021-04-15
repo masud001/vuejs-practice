ef8b8c44172432631a5bcd928bc97139997af4f1
masud001 <madhnagar@gmail.com>
2021-04-16 01:20:55 +0600

    task loop is done

---
+++ todo-app/src/components/Tasks.vue
@@ -0,0 +1,77 @@
+<template>
+  <ul dense bordered padding class="rounded-borders">
+    <li v-for="(task, index) in tasks" :key="task.name">
+      <div class=" q-ma-none text-h6 text-weight-bolder">
+        {{ task.name }}
+        <q-badge color="purple" class=" q-ml-lg "
+          >index is : {{ index }}</q-badge
+        >
+        <q-btn
+          @click="deleteItem(index)"
+          label="delete"
+          color="black"
+          class=" q-ml-md "
+        />
+      </div>
+      <div class="q-ma-none text-weight-thin">
+        Due Date : {{ task.dueDate }}
+      </div>
+      <div class="q-ma-none text-weight-thin">
+        Start Date : {{ task.startDate }}
+      </div>
+      <div class="q-ma-none text-weight-thin">
+        and Time : {{ task.dueTime }}
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["task", "index"],
+  data() {
+    return {
+      tasks: [
+        {
+          name: "task-1",
+          dueDate: "10/04/2021",
+          startDate: "09/04/2021",
+          dueTime: "06:30 AM"
+        },
+        {
+          name: "task - 2",
+          dueDate: "10/04/2021",
+          startDate: "09/04/2021",
+          dueTime: "06:30 AM"
+        },
+        {
+          name: "task - 3",
+          dueDate: "10/04/2021",
+          startDate: "09/04/2021",
+          dueTime: "06:30 AM"
+        },
+        {
+          name: "task - 4",
+          dueDate: "10/04/2021",
+          startDate: "09/04/2021",
+          dueTime: "06:30 AM"
+        },
+        {
+          name: "task - 5",
+          dueDate: "10/04/2021",
+          startDate: "09/04/2021",
+          dueTime: "06:30 AM"
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteItem(index) {
+      console.log("index number : ", index);
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style></style>
--- todo-app/src/pages/Index.vue
+++ todo-app/src/pages/Index.vue
@@ -1,54 +1,76 @@
 <template>
   <q-page class="" padding>
-    <!-- <q-input 
-    v-model="message" 
-    @keyup.esc="clearMessage" 
-    @keyup.enter="alertMessage" 
-    label="Enter Your Name" />
-    <h1 class="text-h4" @click="counter+=1">{{counter}}</h1>
-    <h4 v-if="message.length" class="text-weight-thin text-center border-gray">{{message}}</h4>
-    <h4 v-else class="text-weight-thin text-center border-gray">Write some thing on the text Box...</h4>
-    <h4 class="text-weight-thin text-center border-gray" >{{messageUpperCase }}</h4>
-     <div class="text-center">
-       <q-btn @click="clearMessage"  color="white" text-color="black" label="click Me" />
-     </div> -->
+    <!-- <q-input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      label="Enter Your Name"
+    />
+    <h1 class="text-h4" @click="counter += 1">{{ "click me" }}</h1>
+    <h4 v-if="message.length" class="text-weight-thin text-center border-gray">
+      {{ message }}
+    </h4>
+    <h4 v-else class="text-weight-thin text-center border-gray">
+      Write some thing on the text Box...
+    </h4>
+    <h4 v-show="false">v-show</h4>
+    <h4 class="text-weight-thin text-center border-gray">
+      {{ messageUpperCase() }}
+    </h4>
+    <div class="text-center">
+      <q-btn
+        @click="clearMessage"
+        color="white"
+        text-color="black"
+        label="click Me"
+      />
+    </div> -->
+    <!-- v-for loops  -->
+    <h1 class="text-h6 q-mt-none q-mb-md">v-for loop</h1>
+    <div class="q-pa-md" style="max-width: 550px">
+      <task class="q-pa-md"></task>
+    </div>
   </q-page>
 </template>
 
 // NOTE: Only for js Script
 <script>
+// import Tasks from "components/Tasks.vue";
 export default {
-  // data() {
-  //   return {
-  //     message: "I don't love Vue.js",
-  //     counter: 0
-  //   };
-  // },
-  // computed: {
-  //   messageUpperCase() {
-  //     console.log("fired...");
-  //     return this.message.toUpperCase();
-  //   }
-  // },
-  // methods: {
-  //   clearMessage() {
-  //     this.message = "";
-  //   },
-  //   handelKey(e) {
-  //     console.log("key press event", e);
-  //     if (e.keyCode == 27) {
-  //       this.clearMessage();
-  //     }
-  //   },
-  //   alertMessage() {
-  //     alert(this.message);
-  //   }
-  // },
-  // filters: {
-  //   toloawercase: value => {
-  //     return value.toLowerCase() + "...";
-  //   }
-  // }
+  components: {
+    task: require("components/Tasks.vue").default
+  },
+  data() {
+    return {
+      message: "I don't love Vue.js",
+      counter: 0
+    };
+  },
+  computed: {},
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    handelKey(e) {
+      console.log("key press event", e);
+      if (e.keyCode == 27) {
+        this.clearMessage();
+      }
+    },
+    alertMessage() {
+      console.log("alert");
+      alert(this.message);
+    },
+    messageUpperCase() {
+      console.log("fired...");
+      return this.message.toUpperCase();
+    }
+  },
+  filters: {
+    toloawercase: value => {
+      return value.toLowerCase() + "...";
+    }
+  }
 };
 </script>
 
